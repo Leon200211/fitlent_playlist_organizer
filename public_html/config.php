1441b2437453773bdb3c6397daef6f4087a36658
Leon200211 <leon200207@yandex.ru>
2023-02-11 13:51:59 +0300

    Создал структуру сайта

---
+++ public_html/config.php
@@ -0,0 +1,18 @@
+<?php
+#/
+
+defined('VG_ACCESS') or die('Access denied');
+
+
+// константы
+const SITE_URL = 'http://fitlent.playlist.organizer';
+
+const PATH = '/';
+
+const HOST = 'localhost';
+const HOST = 'localhost';
+const USER = 'root';
+const PASS = 'root';
+const DB_NAME = 'fitlent_playlist_organizer';
+
+
